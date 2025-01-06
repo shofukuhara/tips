33bcff3dec3263e8529e1998ff9ed8f6e6dd4fb0
shofukuhara <sho.fukuhara0213@gmail.com>
2025-01-06 23:49:32 +0900

    fix:アンカーリンク実装中

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
-class fadeEffect {
-  constructor() {
-    this.logo = document.querySelector('[data-logo]');
-  }
-  init() {
-    window.addEventListener('DOMContentLoaded', () => {
-      this._fade();
-    });
-  }
-  _fade() {
-    this.logo.setAttribute('data-logo', 'active');
-  }
-}
+// class fadeEffect {
+//   constructor() {
+//     this.logo = document.querySelector('[data-logo]');
+//   }
+//   init() {
+//     window.addEventListener('DOMContentLoaded', () => {
+//       this._fade();
+//     });
+//   }
+//   _fade() {
+//     this.logo.setAttribute('data-logo', 'active');
+//   }
+// }
 
-const fade = new fadeEffect();
-fade.init();
+// const fade = new fadeEffect();
+// fade.init();
