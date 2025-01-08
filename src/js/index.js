1b90b4733149313d3b1081359b6473db74f3a485
shofukuhara <sho.fukuhara0213@gmail.com>
2025-01-08 23:41:43 +0900

    add:ページ追加

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
 import { ancher } from './module/anchor';
+import { scrollAnimationObserver } from './module/fade';
 
 const anchor = new ancher();
 anchor.init();
+
+const fade = new scrollAnimationObserver();
+fade.init();
---
+++ src/js/module/fade.js
@@ -0,0 +1,52 @@
+import { gsap } from 'gsap';
+export class scrollAnimationObserver {
+  constructor(options = { threshold: 0.5, rootMargin: '0px' }) {
+    // 複数の observer を保存
+    this.observers = [];
+    this.options = options;
+    this.fadeItem = 'data-scroll-fade';
+  }
+
+  // コールバック
+  _handleIntersect(entries) {
+    entries.forEach((entry) => {
+      const target = entry.target;
+      if (entry.isIntersecting) {
+        // 交差した場合のアニメーション
+        if (target.hasAttribute(this.fadeItem)) {
+          gsap.to(target, {
+            duration: 2,
+            ease: 'power2.out',
+            autoAlpha: 1,
+            onComplete: () => {
+              target.setAttribute(this.fadeItem, 'active');
+            },
+          });
+        }
+      }
+    });
+  }
+
+  // 観察する対象要素とオプションを受け取る
+  _createObserver(selector, options = {}) {
+    const observer = new IntersectionObserver(
+      (entries, obs) => this._handleIntersect(entries, obs),
+      { ...this.options, ...options }
+    );
+
+    // 監視対象の要素を指定
+    document.querySelectorAll(selector).forEach((element) => {
+      observer.observe(element);
+    });
+
+    this.observers.push(observer);
+  }
+
+  // 監視を開始
+  init() {
+    this._createObserver(`[${this.fadeItem}]`, {
+      rootMargin: '0px 0px -50% 0px',
+      threshold: 0.5,
+    });
+  }
+}
