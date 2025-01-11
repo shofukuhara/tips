eed0706937e9dc3e19eebb3643a0a21abfec4a2e
shofukuhara <sho.fukuhara0213@gmail.com>
2025-01-11 16:22:58 +0900

    add:非同期処理追加

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 import { ancher } from './module/anchor';
 import { scrollAnimationObserver } from './module/fade';
+// import { load } from './module/load';
+import { item } from './module/asynchronous';
 
 const anchor = new ancher();
 anchor.init();
 
 const fade = new scrollAnimationObserver();
 fade.init();
+
+item();
