af229ce7ac60ab9de9a0da9897a8f23d6d8ad1f4
shofukuhara <sho.fukuhara0213@gmail.com>
2025-01-12 16:38:53 +0900

    fix:記述調整

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
-import { ancher } from './module/anchor';
-import { scrollAnimationObserver } from './module/fade';
-// import { load } from './module/load';
-import { item } from './module/asynchronous';
+import { ancher } from './module/js/page-01';
+import { scrollAnimationObserver } from './module/js/page-02';
+import { loadAnimation } from './module/js/page-03';
 
-const anchor = new ancher();
-anchor.init();
-
-const fade = new scrollAnimationObserver();
-fade.init();
-
-item();
+const page = document.querySelector('[data-page]');
+const pageAttribute = page.getAttribute('data-page');
+if (pageAttribute === 'page01') {
+  const anchor = new ancher();
+  anchor.init();
+} else if (pageAttribute === 'page02') {
+  const fade = new scrollAnimationObserver();
+  fade.init();
+} else if (pageAttribute === 'page03') {
+  new loadAnimation();
+}
---
+++ src/js/module/js/page-03.js
@@ -0,0 +1,31 @@
+import barba from '@barba/core';
+import barbaPrefetch from '@barba/prefetch';
+import { gsap } from 'gsap';
+barba.use(barbaPrefetch);
+
+export class loadAnimation {
+  constructor() {
+    this.init();
+  }
+  init() {
+    barba.init({
+      transitions: [
+        {
+          name: 'default-transition',
+          leave: (data) =>
+            this._animateClipPath(data.current.container, 'circle(0% at 50% 50%)'),
+          enter: (data) =>
+            this._animateClipPath(data.next.container, 'circle(100% at 50% 50%)'),
+        },
+      ],
+    });
+  }
+  _animateClipPath(container, clipPathValue) {
+    const item = container.querySelector('[data-circle]');
+    gsap.to(item, {
+      duration: 1.25,
+      ease: 'power1.out',
+      clipPath: clipPathValue,
+    });
+  }
+}
