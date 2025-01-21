dd614df3696a83ae6ff353ac17c91ebfc866553f
shofukuhara <sho.fukuhara0213@gmail.com>
2025-01-22 01:41:26 +0900

    fix:作業中

--- src/js/module/js/page-09.js
+++ src/js/module/js/page-09.js
@@ -1,28 +1,53 @@
 import Splide from '@splidejs/splide';
-import '@splidejs/splide/css/core';
+import '@splidejs/splide/css';
 
-export class splide {
+export class SplideManager {
   constructor() {
     this.slideItem = document.querySelector('.splide');
-    this.splideInstance = new Splide(this.slideItem);
-    this.spOptions = {
-      type: 'loop',
-      perPage: 1,
+    this.splideInstance = null;
+    this.options = {
+      sp: {
+        type: 'loop',
+        perPage: 1,
+      },
+      pc: {
+        type: 'loop',
+        gap: '1vw',
+        perPage: 3,
+        perMove: 1,
+      },
     };
-    this.pcOptions = {
-      type: 'loop',
-      perPage: 3,
-    };
+    this.mediaQuery = window.matchMedia('(min-width: 768px)');
   }
 
-  applyOptions(options) {
-    this.splideInstance.destroy();
+  getOptions() {
+    let options;
+    if (this.mediaQuery.matches) {
+      options = this.options.pc;
+    } else {
+      options = this.options.sp;
+    }
+    return options;
+  }
+
+  initializeSplide() {
+    const options = this.getOptions();
+    if (this.splideInstance) {
+      this.splideInstance.destroy();
+    }
     this.splideInstance = new Splide(this.slideItem, options);
     this.splideInstance.mount();
   }
+
   init() {
+    // 初期化処理
     window.addEventListener('DOMContentLoaded', () => {
-      applyOptions(pcOptions);
+      this.initializeSplide();
+    });
+
+    // リサイズ時の処理
+    this.mediaQuery.addEventListener('change', () => {
+      this.initializeSplide();
     });
   }
 }
