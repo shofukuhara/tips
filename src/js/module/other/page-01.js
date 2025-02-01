cabf88772b8fe205271fa0fe91ec816be52b856a
shofukuhara <sho.fukuhara0213@gmail.com>
2025-02-01 09:37:23 +0900

    fix:アコーディオンのアニメーション追加

--- src/js/module/other/page-01.js
+++ src/js/module/other/page-01.js
@@ -2,7 +2,7 @@ import { gsap } from 'gsap';
 export class accordion {
   constructor() {
     this.accordion = document.querySelector('[data-accordion]');
-    this.text = document.querySelector('[data-accordion="text"]');
+    this.text = document.querySelector('[data-content]');
   }
 
   init() {
@@ -11,29 +11,37 @@ export class accordion {
       this._toggle();
     });
   }
-
+  _open() {
+    this.accordion.setAttribute('open', '');
+    gsap.fromTo(
+      this.text,
+      {
+        height: 0,
+      },
+      {
+        height: 'auto',
+        duration: 0.8,
+        ease: 'power3.out',
+        overwrite: true,
+        onComplete: () => {
+          this.accordion.setAttribute('data-accordion', 'open');
+        },
+      }
+    );
+  }
   _close() {
     gsap.to(this.text, {
       height: 0,
-      duration: 0.3,
+      duration: 0.8,
       ease: 'power3.out',
       overwrite: true,
       onComplete: () => {
         this.accordion.setAttribute('data-accordion', 'close');
+        this.accordion.removeAttribute('open');
       },
     });
   }
-  _open() {
-    gsap.to(this.text, {
-      height: 'auto',
-      duration: 0.3,
-      ease: 'power3.out',
-      overwrite: true,
-      onComplete: () => {
-        this.accordion.setAttribute('data-accordion', 'open');
-      },
-    });
-  }
+
   _toggle() {
     if (this.accordion.getAttribute('data-accordion') === 'open') {
       this._close();
