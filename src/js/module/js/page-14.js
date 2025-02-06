f376f5c77115532ff21c31c54a70cfc77b729e3f
shofukuhara <sho.fukuhara0213@gmail.com>
2025-02-07 00:08:19 +0900

    fix:canvas勉強中

--- src/js/module/js/page-14.js
+++ src/js/module/js/page-14.js
@@ -6,18 +6,23 @@ export class Canvas {
     this.height = window.innerHeight;
     this.dom.width = this.width / 2;
     this.dom.height = this.height / 2;
+
+    this.image = new Image();
+    this.image.src = '../../public/assets/images/img_01.jpg';
+    this.image.onload = () => this._draw();
   }
 
   init() {
     ['resize', 'scroll', 'load'].forEach((event) => {
       window.addEventListener(event, () => {
-        this._draw();
+        window.addEventListener(event, () => this._draw());
+        console.log(this.image);
       });
     });
   }
 
   _draw() {
-    this.ctx.fillStyle = 'blue';
-    this.ctx.fillRect(0, 0, this.width, this.height); // キャンバスを青で塗りつぶし
+    this.ctx.clearRect(0, 0, this.dom.width, this.dom.height);
+    this.ctx.drawImage(this.image, 100, 100, this.dom.width, this.dom.height);
   }
 }
