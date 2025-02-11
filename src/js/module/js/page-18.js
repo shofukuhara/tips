d636c02df0e1c78da746c19dc937df698b712d96
shofukuhara <sho.fukuhara0213@gmail.com>
2025-02-11 13:24:58 +0900

    fix:一旦コミット

--- src/js/module/js/page-18.js
+++ src/js/module/js/page-18.js
@@ -1,32 +1,40 @@
-import gsap from 'gsap';
+import { gsap } from 'gsap';
 
 export class Hover {
   constructor() {
-    this.button = document.querySelector('[data-button]');
+    this.cursor = document.querySelector('[data-cursor]');
+    this.container = document.querySelector('[data-container]');
+    this.pos = { x: 0, y: 0 };
+    this.mouse = {
+      x: this.pos.x,
+      y: this.pos.y,
+    };
+    // this.speed = 0.5;
   }
 
   init() {
-    this.button.addEventListener('mouseover', () => {
-      this._onHover();
+    window.addEventListener('mousemove', (e) => {
+      this.mouse.x = e.pageX;
+      this.mouse.y = e.pageY;
+      this._mouseOver();
     });
-    this.button.addEventListener('mouseleave', () => {
-      this._unHover();
+
+    this.container.addEventListener('mouseenter', () => {
+      this.cursor.setAttribute('data-cursor', 'mouseOver');
+    });
+    this.container.addEventListener('mouseleave', () => {
+      this.cursor.setAttribute('data-cursor', '');
     });
   }
 
-  _onHover() {
-    gsap.fromTo(
-      this.button,
-      { backgroundColor: '#000', duration: 1, ease: 'power2.out' },
-      { backgroundColor: '#fff', duration: 1, ease: 'power2.out' }
-    );
-  }
-
-  _unHover() {
-    gsap.fromTo(
-      this.button,
-      { backgroundColor: '#FFF', duration: 1, ease: 'power2.out' },
-      { backgroundColor: '#000', duration: 1, ease: 'power2.out' }
-    );
+  _mouseOver() {
+    let cursorSetX = gsap.quickSetter(this.cursor, 'x', 'px');
+    let cursorSetY = gsap.quickSetter(this.cursor, 'y', 'px');
+    gsap.ticker.add(() => {
+      this.pos.x += this.mouse.x - this.pos.x;
+      this.pos.y += this.mouse.y - this.pos.y;
+      cursorSetX(this.pos.x - this.cursor.offsetWidth / 2);
+      cursorSetY(this.pos.y - this.cursor.offsetHeight / 2);
+    });
   }
 }
